0e46e7b7bbd36e69b81d6d14a914732d037a3661
kazuma_ohura <k.ohura0303@gmail.com>
2019-07-21 10:39:57 +0900

    window生成。仮にウィンドウサイズは1280x720で作成しています。

---
+++ PAC-MAN(Bomberland)/Main.h
@@ -0,0 +1,26 @@
+﻿#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <windows.h>
+#include <tchar.h>
+
+const INT Width = 1280; //!ウィンドウの横幅
+const INT Height = 720; //!ウィンドウの縦幅
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+
+/*
+*@brief ウィンドウ生成
+*@param ハンドルインスタンスのポインタ変数
+*@param アプリケーションの名前
+*@return ウィンドウハンドル
+*/
+HWND GenerateWindow(HINSTANCE* hInstance,const TCHAR* API_NAME);
+
+/*
+*@brief メインループ
+*@param MSGのポインタ変数
+*/
+VOID Mainloop(MSG* msg);
+
+#endif
